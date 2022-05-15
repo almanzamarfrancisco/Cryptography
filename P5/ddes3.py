7efd19a776e5fabfeaccb766cdbfeed17343b6e8
Alejandro Almanza <alejandro.almanza@e-vanhost.net>
2022-05-15 14:13:45 -0500

    :construction: P5 Progress: finishig first part of P5

--- P5/ddes3.py
+++ P5/ddes3.py
@@ -1,16 +1,22 @@
 from Crypto.Cipher import DES3
 from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import unpad
 import base64
 
-
-read_file = open('./keys/key.txt', 'rb')
-key = read_file.read()
-read_file.close()
-# print(key)
-cipher = DES3.new(key, DES3.MODE_CFB)
-read_file = open('./messages/message.txt', 'rb')
-ciphertext = read_file.read()
-read_file.close()
-b64 = cipher.iv + cipher.decrypt(ciphertext)
-msg = base64.b64decode(b64)
-print(msg)
+if __name__ == '__main__':
+	read_file = open('./keys/key.txt', 'rb')
+	b64_key = read_file.read()
+	key = base64.b64decode(b64_key)
+	read_file.close()
+	# print(key)
+	cipher = DES3.new(key, DES3.MODE_CFB)
+	read_file = open('./messages/message.txt', 'rb')
+	b64_ciphertext = read_file.read(128)
+	read_file.close()
+	ciphertext = base64.b64decode(b64_ciphertext)
+	msg = cipher.decrypt(ciphertext)
+	try:
+		msg = unpad(msg, 256)
+	except Exception as e:
+		pass
+	print(msg)
--- P5/des3.py
+++ P5/des3.py
@@ -1,23 +1,30 @@
 from Crypto.Cipher import DES3
 from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad
 import base64
-import math, os, sys
+import os
 
-sec = os.urandom(24)
-# Avoid Option 3
-while True:
-	try:
-		key = DES3.adjust_key_parity(sec)
-		f = open("./keys/key.txt", "wb")
-		f.write(key)
-		f.close()
-		break
-	except ValueError:
-		pass
-cipher = DES3.new(key, DES3.MODE_CFB)
-f = open("./messages/message.txt", "wb")
-plaintext = b'All I need is your love tonigth'
-b64 = base64.b64encode(plaintext)
-msg = cipher.iv + cipher.encrypt(b64)
-f.write(msg)
-f.close()
+if __name__ == '__main__':
+	sec = os.urandom(24)
+	# Avoid Option 3
+	while True:
+		try:
+			key = DES3.adjust_key_parity(sec)
+			f = open("./keys/key.txt", "wb")
+			b64_key = base64.b64encode(key)
+			f.write(b64_key)
+			f.close()
+			random_bytes_file = open('/dev/random', 'rb')
+			padding = random_bytes_file.read(1000000)
+			random_bytes_file.close()
+			break
+		except ValueError:
+			pass
+	cipher = DES3.new(key, DES3.MODE_CFB)
+	f = open("./messages/message.txt", "wb")
+	plaintext = b"There once a ship that put to sea..."
+	msg = cipher.iv + cipher.encrypt(pad(plaintext, 256))
+	b64 = base64.b64encode(msg)
+	padding = base64.b64encode(padding)
+	f.write(b64+padding)
+	f.close()
